80237719b7fa9024abcb034c546bb44933ff4464
woolgosh <woolgosh@mail.ru>
2021-12-16 17:08:37 +0300

    first commit

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name= 'город'
+		verbose_name_plural = 'Города'
+		ordering = ['name']
